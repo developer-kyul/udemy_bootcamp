e75f008fd4ae18b911a6c212dfcad9ce58dec38f
Hankyul <developer.kyul@gmail.com>
2023-12-05 17:34:04 +0900

    [study] JS 판단 내리기 - if eles - 삼중등호 vs 이중등호 - and or - Switch

---
+++ JavaScript/03_Boolean_Logic.js
@@ -0,0 +1,27 @@
+//비교연산자 
+//>
+//<
+//>=
+//<=
+//==
+//!=
+//===
+//!==
+
+//삼중 등호 vs 이중 등호
+//==
+//!=
+//===
+//!==
+
+//console
+console.log("hi")
+
+//Alert
+alert("hi")
+
+//Prompt
+prompt("please enter a number")
+
+//IF statement
+
---
+++ JavaScript/03_Boolean_Logic_CODE/Truthy_Falsy/app.js
@@ -0,0 +1,42 @@
+const userInput = prompt("Enter something");
+
+if (userInput) {
+    console.log("TRUTHY!")
+} else {
+    console.log("FALSY!")
+}
+
+// if (0) {
+//     console.log("TRUTHY")
+// } else {
+//     console.log("FALSY")
+// }
+
+// if (NaN) {
+//     console.log("TRUTHY")
+// } else {
+//     console.log("FALSY")
+// }
+
+// if (undefined) {
+//     console.log("TRUTHY")
+// } else {
+//     console.log("FALSY")
+// }
+
+// if (' ') {
+//     console.log("TRUTHY")
+// } else {
+//     console.log("FALSY")
+// }
+
+
+//Falsy Values
+//false
+//0
+//"" (empty string)
+//null
+//undefined
+//NnN
+
+//Everything else is truthy!
