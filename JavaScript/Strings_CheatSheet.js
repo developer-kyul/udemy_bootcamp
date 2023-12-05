a652f9417f6429b6aefdd345b1ccccd3c1a8836c
Hankyul <developer.kyul@gmail.com>
2023-12-05 09:44:23 +0900

    [study] JS 기초 JS 문자열

---
+++ JavaScript/Strings_CheatSheet.js
@@ -0,0 +1,64 @@
+//JS 기초
+
+//number
+//string
+//boolean
+//null
+//undefined
+console.log("hi");
+
+// Numbers:
+1;
+-99;
+0.345345;
+
+//Making variables with let:
+let numberOfFriends = 1;
+
+//Incrementing:
+numberOfFriends += 3; //numberOfFriends is now 4
+
+let score = 30;
+score = score + 1;
+score += 1;
+
+let numLives = 9;
+numLives -= 8;
+
+//+=
+//-=
+//*=
+// /=
+
+// Variables with const
+const minimumAge = 21; //CANNOT REASSIGN!
+
+//Booleans - true or false values
+true;
+false;
+let isHappy = true;
+
+//변수 
+//let const 
+
+//Naming Conventions
+// Use upper camel-cased names(카멜케이스):
+let numberOfChickens = 6; //GOOD
+// NOT THE JS WAY:
+// let number_of_chickens = 6;
+//변수는 의미있는 네임으로 짓기
+
+// i++ vs ++i
+// i++ 
+let i = 0;
+i++;
+
+//먼저 0 반환하고, 1씩 더함
+
+//++i 
+let k = 5;
+let result = ++k;
+result //6
+
+//1을 더하고 반환
+
